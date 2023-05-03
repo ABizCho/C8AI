cf11e534b035bdca63d7cbd3e183a74486dfbaf6
horz.d <he1236@ajou.ac.kr>
2023-05-03 18:47:43 +0900

    back-core: AiTool필드추가 & 카테고리 테이블 분리 (모델, 시리얼라이저 수정)

--- cmd8-back/cmd8_server/core/models.py
+++ cmd8-back/cmd8_server/core/models.py
@@ -1,17 +1,41 @@
 from django.db import models
 # from django.contrib.postgres.fields import JSONField
 
-class CommonInfo(models.Model):
+# from accounts.models import User
+
+### Abstracts
+class AbstractTool(models.Model):
     id = models.AutoField(primary_key=True)
     imgUrl = models.CharField(max_length=255)
-    ko = models.JSONField()
-    en = models.JSONField()
+    name = models.JSONField() # name: {ko: string[], en: string[]}
+    summary = models.TextField()
+    redirectUrl = models.CharField(max_length=255)
     
     class Meta:
         abstract = True
 
-####
-class AiTool(CommonInfo):
-    class Meta(CommonInfo.Meta):
+### models
+class AiToolCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name =  models.JSONField()  # name: { ko: string[], en: string[] }
+    
+class AiTool(AbstractTool):
+    class Meta(AbstractTool.Meta):
         managed = True #db에 테이블을 추가 및 삭제한다.
         db_table = 'tb_aitool' 
+        categories = models.ManyToManyField(AiToolCategory, related_name='ai_tools') #N-M관계, 역참조이름 설정
+        
+
+
+# ### User 기반
+# class AiToolScore(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     tool = models.ForeignKey(AiTool, on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     score = models.FloatField()
+
+# class AiToolFavorite(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     tool = models.ForeignKey(AiTool, on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- cmd8-back/cmd8_server/core/serializers.py
+++ cmd8-back/cmd8_server/core/serializers.py
@@ -1,17 +1,35 @@
 from rest_framework import serializers
-from .models import AiTool
+from .models import AiTool, AiToolCategory
 
-# en, ko 각각은 name과 category 하위 필드(string[])를 가지는 json이다. 
-#   따라서 이들 또한, 하위 직렬화해줘야함.
-class NameCategorySerializer(serializers.Serializer):
-    name = serializers.ListField(child=serializers.CharField())
-    category = serializers.ListField(child=serializers.CharField())
+### Abstracts
+class AbstractBinNameSerializer(serializers.Serializer):
+    ko = serializers.ListField(child=serializers.CharField())
+    en = serializers.ListField(child=serializers.CharField())
 
+    class Meta:
+        abstract = True
+
+
+### Serializers
+#
+class NameSerializer(AbstractBinNameSerializer):
+    pass
+
+class CategoryNameSerializer(AbstractBinNameSerializer):
+    pass
+
+# N-M관계 고려 직렬화
+class AiToolCategorySerializer(serializers.ModelSerializer):
+    name = CategoryNameSerializer()
+
+    class Meta:
+        model = AiToolCategory
+        fields = ('id', 'name')
 
 class AiToolSerializer(serializers.ModelSerializer):
-    ko = NameCategorySerializer()
-    en = NameCategorySerializer()
+    name = NameSerializer()
+    categories = AiToolCategorySerializer(many=True)
 
     class Meta:
         model = AiTool
-        fields = ('id', 'imgUrl', 'ko', 'en')
+        fields = ('id', 'imgUrl', 'name', 'summary', 'redirectUrl', 'categories')
