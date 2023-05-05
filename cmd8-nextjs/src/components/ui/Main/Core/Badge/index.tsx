1ab37a4235e51751baeffe7516a1485bbab8b5fd
horz.d <he1236@ajou.ac.kr>
2023-05-05 19:14:03 +0900

    badge 색상 카드 색상 통일

--- cmd8-nextjs/src/components/ui/Main/Core/Badge/index.tsx
+++ cmd8-nextjs/src/components/ui/Main/Core/Badge/index.tsx
@@ -15,12 +15,12 @@ const Badge = ({
   const badgeImgUrl = `/images/badge/badge-${categoryKey}.png`;
 
   const BadgeVariants: ICategoryVariants = {
-    chat: `bg-cyan-900 hover:bg-cyan-800`,
+    chat: `bg-cyan-800 hover:bg-cyan-700`,
     image: `bg-purple-500 hover:bg-purple-400`,
-    video: `bg-fuchsia-500 hover:bg-fuchsia-400`,
+    video: `bg-violet-500 hover:bg-violet-400`,
     music: `bg-pink-200 hover:bg-pink-100`,
     writing: `bg-amber-500 hover:bg-amber-400`,
-    office: `bg-slate-600 hover:bg-slate-500`,
+    office: `bg-slate-500 hover:bg-slate-400`,
     developer: `bg-zinc-600 hover:bg-zinc-500`,
   };
 
