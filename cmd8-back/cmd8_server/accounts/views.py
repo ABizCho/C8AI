7655761127b67affc094157db79a1a3d4536075b
horz.d <he1236@ajou.ac.kr>
2023-05-11 10:13:38 +0900

    env for test Deploy

--- cmd8-back/cmd8_server/accounts/views.py
+++ cmd8-back/cmd8_server/accounts/views.py
@@ -49,8 +49,8 @@ def login_view(request):
         }, status=status.HTTP_200_OK)
         response.set_cookie('refresh', str(refresh), 
                             httponly=True # 스크립트상 접근불가
-                            , samesite='None' 
-                            , secure=False # 개발환경 테스트 False,
+                            , samesite='Lax' 
+                            , secure=True # 개발환경 테스트 False,
                             # Strict 모드에서는 같은 도메인 범위에서만 해당 쿠키를 사용, Lax는 사용자가 페이지 이동 시 혹은 Form을 통한 Get 요청 시에만 허용함
                             
                             )
--- cmd8-nextjs/src/pages/api/env/HOST.ts
+++ cmd8-nextjs/src/pages/api/env/HOST.ts
@@ -1,3 +1,4 @@
-import { LOCAL_API_HOST } from "./CONSTANTS";
+// import { LOCAL_API_HOST } from "./CONSTANTS";
 
-export const HOST = LOCAL_API_HOST;
+// export const HOST = LOCAL_API_HOST;
+export const HOST = "http://127.0.0.1:8000";
