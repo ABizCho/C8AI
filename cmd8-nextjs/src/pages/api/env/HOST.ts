591bfb5f6bed9d8cde3ae62191e2675b27323fe1
horz.d <he1236@ajou.ac.kr>
2023-05-20 03:12:22 +0900

    docker compose 파일 수정 및 next.js의 요청 API HOST를 서브도메인 api.ai-ght.com 반영

--- cmd8-nextjs/src/pages/api/env/HOST.ts
+++ cmd8-nextjs/src/pages/api/env/HOST.ts
@@ -1,4 +1,5 @@
 // import { LOCAL_API_HOST } from "./CONSTANTS";
 
 // export const HOST = LOCAL_API_HOST;
-export const HOST = "http://127.0.0.1:8000";
+// export const HOST = "http://127.0.0.1:8000";
+export const HOST = "http://api.ai-ght.com";
